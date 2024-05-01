04f60eb0d4129dfc44b55ecfd8a2413280d8b6f6
Siege_Dev <creindl89@gmail.com>
2024-05-01 15:16:44 -0700

    Learning java files

---
+++ variables.java
@@ -0,0 +1,38 @@
+public class variables {
+    public static void main(String[] args) {
+        
+        //Variables
+        String name = "fname";
+        int counter = 0;
+        long big_counter = 0L;
+        float amount = 0.00f;
+        double converted_amt = 0.00;
+        char flag = 'X';
+        boolean testing = true;
+
+        int myNum = 15;
+        myNum = 20;
+        System.out.println(myNum);
+
+        int x, y, z;
+        x = y = z = 50;
+        System.out.println(x + y + z);
+
+        //Primitive Data Types
+        byte b1 = 1;
+        short s1 = 28_000;
+        int i1 = 1_500_000;
+        long l1 = 435345345345L;
+        float f1 = 78.2345F;
+        double d1 = 67.234234234;
+        boolean bl1 = true;
+        char c1 = 'X';
+
+        // Non-primative
+        String name1 = "jeff";
+        // Arrays
+        // Classesd
+
+    }
+    
+}
